6870d70b7bb56c5d627c9b671b716409a1dd78b8
风茗 <yinjuncheng.yjc@alibaba-inc.com>
2025-12-12 10:24:25 +0800

    feat: support dt customize embedding methods

--- rtp_llm/frontend/tokenizer_factory/tokenizers/base_tokenizer.py
+++ rtp_llm/frontend/tokenizer_factory/tokenizers/base_tokenizer.py
@@ -1,5 +1,6 @@
 from typing import Any, Dict, List, Union
 
+import torch
 from transformers import AutoTokenizer
 
 
@@ -21,6 +22,9 @@ class BaseTokenizer:
         if isinstance(token_id, List) and len(token_id) == 0:
             return ""
         return self.tokenizer.decode(token_id, **kwargs)
+
+    def batch_decode(self, token_ids: Union[List[int], List[List[int]], "torch.Tensor"], **kwargs):
+        return self.tokenizer.batch_decode(token_ids, **kwargs)
 
     def apply_chat_template(self, messages, **kwargs):
         return self.tokenizer.apply_chat_template(messages, **kwargs)
